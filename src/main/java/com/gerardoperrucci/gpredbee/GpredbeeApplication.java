55dccebafb05309f671c2098d7eb264544099311
GerardoMac <centrodph@gmail.com>
2018-04-12 06:47:30 -0300

    init

---
+++ src/main/java/com/gerardoperrucci/gpredbee/GpredbeeApplication.java
@@ -0,0 +1,13 @@
+package com.gerardoperrucci.gpredbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpredbeeApplication {
+
+	public static void main(String[] args) {
+		//System.out.println("por aca>>>>>>>>>>>>>>>>>>>>>>>>>");
+		SpringApplication.run(GpredbeeApplication.class, args);
+	}
+}
---
+++ src/main/java/com/gerardoperrucci/gpredbee/controller/StatusController.java
@@ -0,0 +1,18 @@
+package com.gerardoperrucci.gpredbee.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/")
+    public String status() {
+        return "{ \"status\": \"deploy working\" }";
+    }
+}
